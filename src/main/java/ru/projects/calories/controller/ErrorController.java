0a95ec07e2df397b3707536a1509493105df87f0
arekusey-san <ykpon5@ya.ru>
2024-01-30 05:35:46 +0300

    Fix error ExceptionHandler bug

--- src/main/java/ru/projects/calories/controller/ErrorController.java
+++ src/main/java/ru/projects/calories/controller/ErrorController.java
@@ -4,12 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@RequestMapping("/error")
 public class ErrorController
 {
-	@ExceptionHandler(Exception.class)
+	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public String notFoundHandler(Exception e, Model model)
 	{
@@ -18,7 +20,7 @@ public class ErrorController
 		return "errors/error-404";
 	}
 
-	@ExceptionHandler(Exception.class)
+	@ExceptionHandler(InternalServerErrorException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public String internalServerError(Exception e, Model model)
 	{
@@ -26,4 +28,12 @@ public class ErrorController
 
 		return "errors/error-500";
 	}
+}
+
+class NotFoundException extends Exception
+{
+}
+
+class InternalServerErrorException extends Exception
+{
 }
