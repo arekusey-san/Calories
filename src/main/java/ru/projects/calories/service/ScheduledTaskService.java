f3e3fe1892a7d90ce5f6ecc3c12a672ef73b6dbc
NataliN <natali.nickitina2011@yandex.ru>
2024-01-30 21:55:14 +0300

    Создание архива регулярной резервной копии и отправка на почту.

---
+++ src/main/java/ru/projects/calories/service/ScheduledTaskService.java
@@ -0,0 +1,24 @@
+package ru.projects.calories.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduledTaskService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTaskService.class);
+
+    private final BackupService backupService;
+
+    public ScheduledTaskService(BackupService backupService) {
+        this.backupService = backupService;
+    }
+
+    @Scheduled(cron = "${task.cron.expression}", zone = "Europe/Moscow")
+    public void performScheduledTask() {
+        LOGGER.info("Scheduled task started.");
+        backupService.createBackupAndSendEmail();
+        LOGGER.info("Scheduled task completed.");
+    }
+}
